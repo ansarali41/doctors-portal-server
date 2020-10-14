2addf44bb706907cf0cf770506632f2801e859f4
ansarali41 <ansar.ict14@gmail.com>
2020-10-14 10:54:15 +0600

    ready to deploy on heroku

--- index.js
+++ index.js
@@ -96,6 +96,4 @@ client.connect(err => {
 });
 
 const port = 5000;
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
